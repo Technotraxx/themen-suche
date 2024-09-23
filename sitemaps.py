94a94e102d5a832960f2679ff7dcbd98f4683abc
Technotraxx <114928644+Technotraxx@users.noreply.github.com>
2024-09-23 18:54:11 +0200

    Create sitemaps.py

---
+++ sitemaps.py
@@ -0,0 +1,6 @@
+SITEMAP_LIBRARY = {
+    'Stern.de News Sitemap': 'https://www.stern.de/736974656d6170-news.xml',
+    'Stern.de Video Sitemap': 'https://www.stern.de/736974656d6170-video.xml',
+    'Welt.de Sitemap': 'https://www.welt.de/sitemaps/welt/sitemap.xml',
+    # Fügen Sie hier weitere Sitemaps hinzu
+}
