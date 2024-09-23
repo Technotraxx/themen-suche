cf65575645d34b53c63d330ad760511d1eb5bf4e
Technotraxx <114928644+Technotraxx@users.noreply.github.com>
2024-09-23 18:54:25 +0200

    Create kategorien.py

---
+++ kategorien.py
@@ -0,0 +1,6 @@
+BEKANNTE_KATEGORIEN = [
+    'politik', 'wirtschaft', 'kultur', 'sport', 'finanzen', 'gesellschaft',
+    'wissen', 'gesundheit', 'technik', 'auto', 'reisen', 'digital', 'panorama',
+    'wissenschaft', 'regional', 'meinung', 'video', 'bilder', 'leben', 'stil',
+    # Fügen Sie hier weitere bekannte Kategorien hinzu
+]
