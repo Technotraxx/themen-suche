3d2d3f8c976abdc9ffeea70ac2247824aa66674d
Technotraxx <114928644+Technotraxx@users.noreply.github.com>
2024-10-03 12:43:50 +0200

    Update sitemaps.py

--- sitemaps.py
+++ sitemaps.py
@@ -2,6 +2,9 @@ SITEMAP_LIBRARY = {
     'Stern.de News Sitemap': 'https://www.stern.de/736974656d6170-news.xml',
     'Welt.de News Sitemap': 'https://www.welt.de/sitemaps/newssitemap/newssitemap.xml',
     'Spiegel.de News Sitemap': 'https://www.spiegel.de/sitemaps/news-de.xml',
-    'Focus.de Politik News Sitemap': 'https://www.focus.de/sitemap_news_politik.xml'
+    'Focus.de Politik News Sitemap': 'https://www.focus.de/sitemap_news_politik.xml',
+    'Bild.de News Sitemap': 'https://www.bild.de/sitemap-news.xml',
+    'Tagesschau.de Sitemap': 'https://www.tagesschau.de/index~atom.xml',
+    'T-Online.de RSS Feed': 'https://www.t-online.de/schlagzeilen/feed.rss'
     # Fügen Sie hier weitere Sitemaps hinzu
 }
