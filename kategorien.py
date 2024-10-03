10147bc4a073a430becd7719fcdf52f77e2985d8
Technotraxx <114928644+Technotraxx@users.noreply.github.com>
2024-10-03 13:56:19 +0200

    Update kategorien.py

--- kategorien.py
+++ kategorien.py
@@ -5,5 +5,10 @@ BEKANNTE_KATEGORIEN = {
     'meinung', 'panorama', 'regional', 'wetter', 'ratgeber', 'video', 'podcast', 'bilder',
     'international', 'inland', 'ausland', 'europa', 'usa', 'asien', 'afrika', 'umwelt',
     'nachhaltigkeit', 'kunst', 'musik', 'film', 'fernsehen', 'literatur', 'theater',
-    'genuss', 'wohnen', 'garten', 'tiere', 'shopping', 'angebote', 'services'
+    'genuss', 'wohnen', 'garten', 'tiere', 'shopping', 'angebote', 'services',
+    # Newly added categories
+    'aktuelles', 'ausflug', 'aussenpolitik', 'deals', 'deutschland', 'essen-trinken', 'heim-garten',
+    'innenpolitik', 'international-politik', 'klima', 'krisen', 'leben-umwelt', 'lokalsport', 
+    'mobilitaet', 'mode-deals', 'muenchen', 'nachrichten', 'parteien', 'region', 'royals', 'spiele',
+    'stars', 'ukraine', 'verkehr-energie'
 }
